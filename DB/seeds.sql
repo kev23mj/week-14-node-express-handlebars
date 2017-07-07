f9a3a3e583cb99200b2a08c193e9a75cf6db9780
kev23mj <kevinsmartabbey@gmail.com>
2017-07-07 06:10:07 -0700

    updated

---
+++ DB/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers_tbl(burger_name) values ('Cheese Burger');
+insert into burgers_tbl(burger_name) values ('Reuben Burger');
+insert into burgers_tbl(burger_name) values ('Bacon Egg and Cheese Burger');
+insert into burgers_tbl(burger_name) values ('Garlic Burger');
